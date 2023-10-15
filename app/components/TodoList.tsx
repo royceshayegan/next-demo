0d003ee55c60368865d0e75b1a30f56b79e40970
royceshayegan <royceshayegan@gmail.com>
2023-10-15 01:12:47 -0400

    auto focus on todolist filter

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -9,7 +9,7 @@ export default function TodoList() {
     <>
       <Window title="Todo List" color="primary" dismissable="back">
         <Button.Group>
-          <input type="text" placeholder="Start typing to filter..." />
+          <input autoFocus type="text" placeholder="Start typing to filter..." />
           <Button disabled>Clear</Button>
         </Button.Group>
         <Table>

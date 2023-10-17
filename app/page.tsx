e3aeef5cec928d752fa5c76b2c5da5c49a367790
royceshayegan <royceshayegan@gmail.com>
2023-10-17 10:13:08 -0400

    changed padding for page container to be better on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import TodoList from "@/app/components/TodoList";
 export default async function Home() {
   return (
     <>
-      <div className="flex justify-center pt-24 lg:pt-48 items-start mx-[4%]">
+      <div className="flex justify-center py-16 lg:py-48 items-start mx-[4%]">
         <div className="max-w-2xl flex flex-col justify-center items-center h-full w-full">
           <TodoList></TodoList>
         </div>
